71f950c01483bc27cb47b1b544c3d70ce4e12e30
KhOo1 <hihe0307@gmail.com>
2026-06-21 18:20:22 -0700

    Update dom.js

--- js/dom.js
+++ js/dom.js
@@ -1,1 +1,15 @@
+const title = document.getElementById("title");
+const btnChange = document.getElementById("btnChange");
 
+btnChange.addEventListener("click", function(){
+
+  // đổi title thành "Học DOM thật vui!"
+  title.textContent = "Học DOM thật vui!";
+
+  // đổi màu chữ
+  title.style.color = "green";
+
+  // đổi màu nền
+  title.style.backgroundColor = "lightyellow";
+
+});
